b4ca4e4ff862eaa8c5fd238cf7fae46575f08221
Haseeb Malik <m.haseebmalik123@gmail.com>
2021-09-23 15:30:54 +0500

    adding a basic gatsby site on netlify server

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,15 @@
+require("dotenv").config({path:".env"})
 module.exports = {
     siteMetadata: {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        {
+            resolve: 'gatsby-plugin-snipcartv3',
+            options: {
+              apiKey: process.env.SNIPCART_KEY
+            }
+          }
+      
     ]
 }
